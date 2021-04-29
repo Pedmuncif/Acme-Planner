82804ec0c9d86017015d374c755abf7978f20dc5
pedmuncif <pedmuncif@alum.us.es>
2021-04-29 17:32:38 +0200

    Task-04: Creacion de la entidad Task hecha.

---
+++ src/main/java/acme/entities/tasks/Task.java
@@ -0,0 +1,55 @@
+package acme.entities.tasks;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.DomainEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Task extends DomainEntity{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@NotEmpty
+	@Length(max=80)
+	protected String title;
+	
+	@NotBlank
+	@Length(max=500)
+	protected String description;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date startMoment;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	protected Date finishMoment;
+	
+	@NotNull
+	@Digits(integer=3,fraction=2)
+	protected Double workload;
+	
+	@URL
+	protected String link;
+	
+	@NotNull
+	protected Boolean status;
+}
